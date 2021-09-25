6896500a6fceff416544f636e416e064710d7ccb
davilav <davilav@unal.edu.co>
2021-09-25 15:58:14 -0500

    MVVM Pattern Design Added

---
+++ app/src/main/java/com/davilav/wigilabstest/ui/movie/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.davilav.wigilabstest.ui.movie
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.davilav.wigilabstest.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
